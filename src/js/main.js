6660ea9695da461a2cfe8ddcdeab1abcad963e44
Jonathan Silva <silva.jon86@gmail.com>
2017-03-16 09:01:51 -0400

    installed angular and angular ui

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import angular from 'angular';
+import 'angular-ui-router';
+
+// import AppConfig from './config';
+
+//IMPORT CONTROLLERS
+
+angular
+	.module('app', ['ui.router'])
+	// .config(AppConfig)
